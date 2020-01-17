ed4cb8ede993c21ae56c6033642107b7418139e6
baf62495 <baf62495@aol.com>
2020-01-17 16:27:46 -0500

    / router set up

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
 
+import LandingPage from './components/LandingPage';
+
 class App extends React.Component {
+  renderMainRoutes() {
+    return (
+      <>
+        <Route path='/logs' />
+        <Route path='/reflections' />
+      </>
+    )
+  }
+
   render() {
     return (
-      <main className='App'>
-        <nav className="landing-nav">
-          <div className="title">MindJournal</div>
-        </nav>
-        <div className="landing-container">
-          <h1>All-in-one Journal for Individuals</h1>
-          <p>Enjoy a healthy, mindful, and balanced life</p>
-          <button>Enter</button>
-        </div>
-      </main>
+      <div className='App'>
+        <Route exact path='/' component={LandingPage} />
+      </div>
     );
   }
 }
---
+++ src/components/LandingPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function LandingPage() {
+    return (
+        <main className='landing-page'>
+            <nav className='landing-nav'>
+                <div className='title'>MindJournal</div>
+            </nav>
+            <div className='landing-container'>
+                <h1>All-in-one Journal for Individuals</h1>
+                <p>Enjoy a healthy, mindful, and balanced life</p>
+                <button>Enter</button>
+            </div>
+        </main>
+    )
+}
