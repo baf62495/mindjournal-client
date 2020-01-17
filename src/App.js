c60a382c386bcd6f40457ded9719102bb2d6d123
baf62495 <baf62495@aol.com>
2020-01-17 16:05:32 -0500

    landing page setup and react router setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import './App.css';
 
-function App() {
-  return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <main className='App'>
+        <nav className="landing-nav">
+          <div className="title">MindJournal</div>
+        </nav>
+        <div className="landing-container">
+          <h1>All-in-one Journal for Individuals</h1>
+          <p>Enjoy a healthy, mindful, and balanced life</p>
+          <button>Enter</button>
+        </div>
+      </main>
+    );
+  }
 }
 
 export default App;
