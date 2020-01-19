7151b6a788c378d9f2f2a8c0801b29209fc6dfe6
baf62495 <baf62495@aol.com>
2020-01-19 15:57:54 -0500

    fontawesome icons

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faBars,
+    faFilter
+} from '@fortawesome/free-solid-svg-icons';
 
 export default function AppHeader(props) {
     return (
         <header className='app-header'>
             <div className="header-left">
-                <button className="js-btn-menu">Menu</button>
+                <button className="js-btn-menu"><FontAwesomeIcon icon={faBars}/></button>
             </div>
             <div className="header-center">{props.title}</div>
             <div className="header-right">
-                <button className="js-btn-filter">Filter</button>
+                <button className="js-btn-filter"><FontAwesomeIcon icon={faFilter}/></button>
             </div>
         </header>
     )
--- src/components/LogItem.js
+++ src/components/LogItem.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faLaughBeam,
+    faSmile,
+    faMeh,
+    faFrown,
+    faTired
+} from '@fortawesome/free-solid-svg-icons';
 
 export default function LogItem(props) {
+    const icons = {
+        'Very Good': faLaughBeam,
+        'Good': faSmile,
+        'Neutral': faMeh,
+        'Angry': faTired,
+        'Sad': faFrown
+    }
     return (
         <div className='mj-card'>
+            <header>
+                <ul className='mj-card-meta'>
+                    <li>{props.created_at}</li>
+                    <li><FontAwesomeIcon icon={icons[props.mood]} /></li>
+                    <li>{props.mood}</li>
+                </ul>
+            </header>
             <p>{props.content}</p>
-            <p>{props.mood}</p>
-            <p>{props.created_at}</p>
         </div>
     )
 }
