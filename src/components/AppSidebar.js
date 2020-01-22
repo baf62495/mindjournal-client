770abbfe413b7638010575356dbc28ad09fde07f
baf62495 <baf62495@aol.com>
2020-01-22 16:31:05 -0500

    sidebar basic style

--- src/components/AppSidebar.js
+++ src/components/AppSidebar.js
@@ -4,7 +4,6 @@ import SearchInput from './SearchInput';
 export default function AppSidebar(props) {
     return (
         <aside className='app-sidebar'>
-            <p>This is the AppSidebar component</p>
             <SearchInput updateSearchTerm={props.updateSearchTerm} />
         </aside>
     )
--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -3,8 +3,9 @@ import React from 'react';
 export default class SearchInput extends React.Component {
     render() {
         return (
-            <div className="SearchBox">
+            <div className="app-sidebar-search">
                 <input
+                    className="search-input"
                     placeholder="Search"
                     value={this.props.searchTerm}
                     onChange={e => this.props.updateSearchTerm(e.target.value)}
