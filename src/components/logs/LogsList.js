180615633851d843f232a29eb5f0af283fa24e7b
baf62495 <baf62495@aol.com>
2020-01-22 20:03:52 -0500

    added conditional rendering for no results

--- src/components/logs/LogsList.js
+++ src/components/logs/LogsList.js
@@ -8,6 +8,15 @@ export default class LogsList extends React.Component {
             .filter(log => log.content.toLowerCase().includes(searchTerm.toLowerCase())
                 && (filterOptions === 'All' || log.mood === filterOptions))
             .map((log, key) => <LogsListItem {...log} key={key} deleteLog={this.props.deleteLog} />);
+
+        if (list.length === 0) {
+            return (
+                <div className='mj-card-list'>
+                    <div className='mj-card-list-title'>Recent Logs</div>
+                    <p><i>Sorry, there are no logs matching this criteria.</i></p>
+                </div>
+            )
+        }
         return (
             <div className='mj-card-list'>
                 <div className='mj-card-list-title'>Recent Logs</div>
--- src/components/reflections/ReflectionsList.js
+++ src/components/reflections/ReflectionsList.js
@@ -7,6 +7,13 @@ export default class LogsList extends React.Component {
         const list = this.props.reflections
             .filter(reflection => reflection.title.toLowerCase().includes(searchTerm.toLowerCase()))
             .map((reflection, key) => <ReflectionsListItem {...reflection} key={key} />);
+        if (list.length === 0) {
+            return (
+                <div className='mj-refl-list'>
+                    <p><i>Sorry, there are no logs matching this criteria.</i></p>
+                </div>
+            )
+        }
         return (
             <div className='mj-refl-list'>
                 {list}
