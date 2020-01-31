608fe7520fab910635ab8ee6e0d9d1c5576891bf
baf62495 <baf62495@aol.com>
2020-01-31 13:27:54 -0500

    updated and styled landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,14 +2,18 @@ import React from 'react';
 
 export default function LandingPage(props) {
     return (
-        <main className='landing-page text-center'>
-            <nav className='landing-nav'>
-                <div className='title'>MindJournal</div>
-            </nav>
-            <div className='landing-container'>
-                <h1>All-in-one Journal for Individuals</h1>
-                <p>Enjoy a healthy, mindful, and balanced life</p>
-                <button onClick={e => props.history.push('/logs')}>Enter</button>
+        <main className='landing-page'>
+            <header className='app-header'>
+                <div className='header-left'>
+                    <div className='main-logo'>MindJournal</div>
+                </div>
+            </header>
+            <div className='mj-page'>
+                <div className='landing-hero text-center'>
+                    <h1>All-in-one Journal for Individuals</h1>
+                    <p className='size-l'>One stop shop for all your journaling needs</p>
+                    <button onClick={e => props.history.push('/logs')}>Start Journaling</button>
+                </div>
             </div>
         </main>
     )
